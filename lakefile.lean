003879c006190ade18bf8980612e9bb8789ec33e
Luca Leon Happel <luca.happel@hhu.de>
2024-02-27 00:29:09 +0100

    add: mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -4,6 +4,14 @@ open Lake DSL
 package «mathcraft» where
   -- add package configuration options here
 
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`pp.proofs.withType, false⟩
+  ]
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 lean_lib «Mathcraft» where
   -- add library configuration options here
 
